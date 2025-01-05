985180bb566726cb2aca4509bccbc23b31dff96a
RapiL <sjw3090@gmail.com>
2025-01-05 11:59:14 +0900

    create 성공 설정

--- src/api-endpoint/api-endpoint.service.spec.ts
+++ src/api-endpoint/api-endpoint.service.spec.ts
@@ -1,18 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ApiEndpointService } from './api-endpoint.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ApiEndpoint } from './entities/api-endpoint.entity';
+import { ApiResponse } from 'src/api-response/entities/api-response.entity';
+import { CreateApiEndpointDto } from './dto/create-api-endpoint.dto';
+
+const mockApiEndpointRepository = {
+  create: jest.fn(),
+  findOne: jest.fn(),
+  find: jest.fn(),
+  delete: jest.fn(),
+  save: jest.fn()
+};
+
+const mockApiResponseRepository = {
+  save: jest.fn()
+};
+
+const mockTimersMap = new Map<number, NodeJS.Timeout>();
 
 describe('ApiEndpointService', () => {
-  let service: ApiEndpointService;
+  let apiEndpointService: ApiEndpointService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ApiEndpointService],
+      providers: [
+        ApiEndpointService,
+        {
+          provide: getRepositoryToken(ApiEndpoint),
+          useValue: mockApiEndpointRepository,
+        },
+        {
+          provide: getRepositoryToken(ApiResponse),
+          useValue: mockApiResponseRepository,
+        },
+        {
+          provide: 'TIMERS_MAP',
+          useValue: mockTimersMap,
+        }
+      ],
     }).compile();
 
-    service = module.get<ApiEndpointService>(ApiEndpointService);
+    apiEndpointService = module.get<ApiEndpointService>(ApiEndpointService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(apiEndpointService).toBeDefined();
   });
+
+
+describe('create', () => {
+  it('api 생성', async() => {
+    
+    const createApiEndpointDto : CreateApiEndpointDto = { 
+      url: 'test@test.com',
+      parameters: [
+        {"type" : "query", "key" : "pageNo", "value" : "1"},
+        {"type" : "apiKey", "key" : "serviceKey", "value" : "R3fWxDee7P9ysC5ty+6Y7LbJyFTiH0ToWmOtlRCJVUdWYd1kAkDzzTS9RA6Mn8Ikq0GYE1eEu462kax9JgnaNw=="}
+      ],
+      callTime: 5000
+    };
+
+    const result = { id: 1, url: createApiEndpointDto.url, parameters: createApiEndpointDto.parameters, callTime: createApiEndpointDto.callTime };
+
+    jest.spyOn(mockApiEndpointRepository, 'findOne')
+    .mockResolvedValueOnce(null)
+    .mockResolvedValueOnce(result);
+    jest.spyOn(mockApiEndpointRepository, 'create').mockResolvedValue(result);
+    jest.spyOn(mockApiEndpointRepository, 'save').mockResolvedValue(result);
+    jest.spyOn(mockApiEndpointRepository, 'findOne').mockResolvedValue(result);
+
+    const createdApiEndpoint = await apiEndpointService.create(createApiEndpointDto);
+
+    expect(createdApiEndpoint).toEqual(result);
+    expect(mockApiEndpointRepository.findOne).toHaveBeenCalledWith({
+      where: {
+        url: createApiEndpointDto.url,
+      }
+    });
+    expect(mockApiEndpointRepository.create).toHaveBeenCalledWith(createApiEndpointDto);
+    expect(mockApiEndpointRepository.save).toHaveBeenCalledWith(result);
+  });
+
+
+})
+
 });
