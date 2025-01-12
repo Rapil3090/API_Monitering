a3cea6766ed341be75921be92d6a665b818de0ad
RapiL <sjw3090@gmail.com>
2025-01-12 22:07:49 +0900

    마이그레이션에 사용된 파일 삭제

--- src/1/repositories/ApiUrlRepository.js
+++
@@ -1,6 +0,0 @@
-const AppDataSource = require('../data-source');
-const ApiUrl = require('../entities/ApiUrl');
-
-const apiUrlRepository = AppDataSource.getRepository('ApiUrl');
-
-module.exports = apiUrlRepository;
