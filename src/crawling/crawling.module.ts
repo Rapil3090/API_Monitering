ce733b30f4de82904aef2ba20a3f12de67d90639
RapiL <sjw3090@gmail.com>
2025-03-01 22:34:00 +0900

    apiendpoint, url 레포 추가

--- src/crawling/crawling.module.ts
+++ src/crawling/crawling.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CrawlingService } from './crawling.service';
 import { CrawlingController } from './crawling.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiEndpoint } from 'src/api-endpoint/entities/api-endpoint.entity';
+import { ApiEndpointRepository } from 'src/api-endpoint/repository/api-endpoint.repository';
+import { UrlRepository } from 'src/api-endpoint/repository/url.repository';
+import { Url } from 'src/api-endpoint/entities/url.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      ApiEndpoint,
+      Url,
+    ]),
+  ],
   controllers: [CrawlingController],
-  providers: [CrawlingService],
+  providers: [
+    CrawlingService,
+    ApiEndpointRepository,
+    UrlRepository,
+  ],
 })
 export class CrawlingModule {}
